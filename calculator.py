1b7f2e9c4be6aaff246df16900b7b9ce8afc5807
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-22 16:21:43 -0800

    WIP Making code more readable

--- calculator.py
+++ calculator.py
@@ -13,6 +13,15 @@ while True:
 
     if user_input == 'q':
         break
+    elif tokens.len() == 2:
+        operation = tokens[0]
+        num1 = int(tokens[1])
+
+        if operation == 'square':
+            print square(num1)
+        elif operation == 'cube':
+            print cube(num1)
+
     elif tokens[0] == "+":
         print add(int(tokens[1]), int(tokens[2]))
     elif tokens[0] == '-':
@@ -21,9 +30,8 @@ while True:
         print multiply(int(tokens[1]), int(tokens[2]))
     elif tokens[0] == '/':
         print divide(int(tokens[1]), int(tokens[2]))
-    elif tokens[0] == 'square':
-        print square(int(tokens[1]))
-    elif tokens[0] == 'cube':
-        print cube(int(tokens[1]))
+
     elif tokens[0] == 'pow':
         print power(int(tokens[1]), int(tokens[2]))
+    elif tokens[0] == 'mod':
+        print mod(int(tokens[1]), int(tokens[2])) 
