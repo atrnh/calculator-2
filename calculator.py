1a368352eb96dd0a65f8e40e444853e8c982efec
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-22 16:09:15 -0800

    user can do cube now

--- calculator.py
+++ calculator.py
@@ -23,3 +23,5 @@ while True:
         print divide(int(tokens[1]), int(tokens[2]))
     elif tokens[0] == 'square':
         print square(int(tokens[1]))
+    elif tokens[0] == 'cube':
+        print cube(int(tokens[1]))
