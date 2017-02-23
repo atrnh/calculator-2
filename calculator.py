61eb5e7bf258ec2b2cf7e04027e269427e05d247
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-22 16:00:59 -0800

    user can subtract numbers now

--- calculator.py
+++ calculator.py
@@ -16,3 +16,5 @@ while True:
     else:
         if tokens[0] == "+":
             print add(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == '-':
+            print subtract(int(tokens[1]), int(tokens[2]))
