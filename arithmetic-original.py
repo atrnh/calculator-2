fd3a0987a3a9d623f64b78c315568043d976b1dd
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-22 18:00:28 -0800

    WIP allow user to put in multiple numbers and not just two numbers

--- arithmetic-original.py
+++ arithmetic-original.py
@@ -1,37 +1,30 @@
-def add(num_list):
-    """Returns sum of all integers in list"""
-    return reduce((lambda x, y: x + y), num_list)
-
+def add(num1, num2):
+    """Return the sum of two numbers"""
+    return num1 + num2
 
 def subtract(num1, num2):
     """Return the difference of two numbers"""
     return num1 - num2
 
-
 def multiply(num1, num2):
     """Return the product of two numbers"""
     return num1 * num2
-
 
 def divide(num1, num2):
     """Return the quotient of two numbers as a float"""
     return float(num1) / float(num2)
 
-
 def square(num):
     """Return the square of a number"""
     return num * num
-
 
 def cube(num):
     """Return the cube of a number"""
     return num ** 3
 
-
 def power(num, exponent):
     """Return num raised to the power of exponent"""
     return num ** exponent
-
 
 def mod(num1, num2):
     """Return remainder of num1 divided by num2"""
--- calculator.py
+++ calculator.py
@@ -14,26 +14,38 @@ while True:
     if user_input == 'q':
         break
     else:
-        operator = tokens[0]
-        try:
-            num1 = int(tokens[1])
-            num2 = int(tokens[2])
-        except IndexError:
-            num1 = int(tokens[1])
+        operator = tokens.pop(0)
+        num_list = list(map(lambda x: int(x), tokens))
+        # num_list = [int(num) for num in tokens]
+
+        # try:
+        #     num1 = int(tokens[1])
+        #     num2 = int(tokens[2])
+        # except IndexError:
+        #     num1 = int(tokens[1])
 
         if operator == "+":
             print add(num1, num2)
-        elif operator == '-':
-            print subtract(num1, num2)
-        elif operator == '*':
-            print multiply(num1, num2)
-        elif operator == '/':
-            print divide(num1, num2)
-        elif operator == 'square':
-            print square(num1)
-        elif operator == 'cube':
-            print cube(num1)
-        elif operator == 'pow':
-            print power(num1, num2)
-        elif operator == 'mod':
-            print mod(num1, num2)
+            # TO DO:
+            # change arguments in add() to reflect
+            # changes made in arithmetic.py
+            # add now takes a list of integers
+
+        # TO DO:
+        # all of this other stuff in the bottom
+        # is broken now
+
+        # elif operator == '-':
+        #     print subtract(num1, num2)
+        # elif operator == '*':
+        #     print multiply(num1, num2)
+        # elif operator == '/':
+        #     print divide(num1, num2)
+        # elif operator == 'square':
+        #     print square(num1)
+        # elif operator == 'cube':
+        #     print cube(num1)
+        # elif operator == 'pow':
+        #     print power(num1, num2)
+        # elif operator == 'mod':
+        #     print mod(num1, num2)
